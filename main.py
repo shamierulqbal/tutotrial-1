ceb3c150b8d76acf6e46f24ca3b6e50dc547d0c7
shamierulqbal <s22a0027@siswa.umk.edu.my>
2025-10-04 11:51:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
- import streamlit as st
+import streamlit as st
 
-  st.title("MUHAMMAD SHAMIERUL IQBAL BIN MOHD SHAMSUDDIN")
-  st.header("Contact Information")
-  st.write("Email: SHAMIERULQBAL@GMAIL.COM")
-  st.write("Phone: 010-278-5963")
-  st.write("LinkedIn: linkedin.com/in/yourprofile")
+st.title("MUHAMMAD SHAMIERUL IQBAL BIN MOHD SHAMSUDDIN")
+st.header("Contact Information")
+st.write("Email: SHAMIERULQBAL@GMAIL.COM")
+st.write("Phone: 010-278-5963")
+st.write("LinkedIn: linkedin.com/in/yourprofile")
 
-  st.header("Education")
-  st.write("Bachelor of Information Technology (Hons), University Malaysia Kelantan, Year 4")
+st.header("Education")
+st.write("Bachelor of Information Technology (Hons), University Malaysia Kelantan, Year 4")
 
-  st.header("Work Experience")
-  st.write("Salesman, 7 Eleven Company, 2019")
+st.header("Work Experience")
+st.write("Salesman, 7 Eleven Company, 2019")
   
 
-  st.header("Skills")
-  st.write("- AI Specialist")
-  st.write("- Machine learning Development")
-  st.write("- UIUX Development")
+st.header("Skills")
+st.write("- AI Specialist")
+st.write("- Machine learning Development")
+st.write("- UIUX Development")
 
-  st.header("Projects")
-  st.write("DIETLAH: FOR NUTRITION AND CALORIES TRACKING ")
-  st.write("-This Project focus for healty life and help people for reduce weight")
+st.header("Projects")
+st.write("DIETLAH: FOR NUTRITION AND CALORIES TRACKING ")
+st.write("-This Project focus for healty life and help people for reduce weight")
